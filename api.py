c06e3ad0543ca6d96a6e492b8cd36395c87910ec
JoseNapitupulu <josemourinhonapitupulu@gmail.com>
2026-03-01 18:35:58 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,27 @@
+import subprocess
+import os
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.wsgi import WSGIMiddleware
+from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "OK"}
+
+# Jalankan Streamlit sebagai subprocess
+if os.getenv("STREAMLIT_RUNNING") != "true":
+    os.environ["STREAMLIT_RUNNING"] = "true"
+    subprocess.Popen([
+        "streamlit",
+        "run",
+        "app.py",
+        "--server.port",
+        os.environ.get("PORT", "8000"),
+        "--server.address",
+        "0.0.0.0",
+        "--server.headless",
+        "true"
+    ])
